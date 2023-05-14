eb42b355f0a901f2582bd610766fad5de90208ee
Khalid Alnasser <loody170ss@gmail.com>
2023-05-14 17:20:19 +0300

    added fetch API function

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function requestAlbumXHR() {
             //processAlbumRequest(req.responseText);
             let response = JSON.parse(req.responseText)
             for(item of response.data){
+                console.log(item)
                 let imgElem = document.createElement("img");
                 imgElem.src = item.link;
 
@@ -25,4 +26,30 @@ function requestAlbumXHR() {
     req.open('GET', 'https://api.imgur.com/3/album/' + albumId + '/images', true); // true for asynchronous     
     req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
     req.send();
+}
+
+function requestAlbumFetch(){
+    let albumId = document.getElementById("albumIdField").value;
+    let resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "";
+    fetch(`https://api.imgur.com/3/album/${albumId}/images`, {
+  method: 'GET',
+  headers: {
+    Authorization: `Client-ID ${clientId}`
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    for(item of data.data){
+        console.log(item)
+        let imgElem = document.createElement("img");
+        imgElem.src = item.link;
+        resultDiv.appendChild(imgElem);
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
 }
